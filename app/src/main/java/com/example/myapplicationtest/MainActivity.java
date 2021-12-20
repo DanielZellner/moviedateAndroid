8e4e248d9315543cbd702fd5a6e7b530d43cc396
DanielZellner <daniel.zellner@edu.fh-joanneum.at>
2021-12-20 20:56:26 +0100

    added things

--- app/src/main/java/com/example/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.java
@@ -1,13 +1,10 @@
 package com.example.myapplicationtest;
 
-import android.annotation.SuppressLint;
 import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -16,8 +13,9 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplicationtest.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     int counter = 2;
     public ImageView imageView;
     private ActivityMainBinding binding;
@@ -38,32 +36,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-        Button button1 = findViewById(R.id.button11);
-        Button button2 = findViewById(R.id.button12);
-
-        imageView = findViewById(R.id.imageView3);
-
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-    }
-
-    @SuppressLint("NonConstantResourceId")
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button11:
-            case R.id.button12:
-                switchPhoto();
-                break;
-        }
-    }
-
-    public void switchPhoto() {
-        if (counter % 2 == 0) {
-            imageView.setImageIcon(Icon.createWithContentUri("@drawable/_002"));
-        } else {
-            imageView.setImageIcon(Icon.createWithContentUri("@drawable/_001"));
-        }
-        counter++;
     }
 }
